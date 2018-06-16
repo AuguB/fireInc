a38bdf44a1b08afc62d68fb329d2f3f70dccd714
guus <guus@Guusbook>
2018-06-16 15:42:31 +0200

    Thir commit

--- src/fireinc/strategies/HiringStrategy.java
+++ src/fireinc/strategies/HiringStrategy.java
@@ -1,4 +1,21 @@
 package fireinc.strategies;
 
+import fireinc.workers.Coder;
+import fireinc.workers.Employee;
+
 public interface HiringStrategy {
+
+    public String getDivisionID();
+    
+    public default Employee hire(int ID){
+        boolean hired = false;
+        Employee current = new Coder("John", this.getDivisionID()+ID);
+        while(!hired){
+            current = new Coder("John", "P1");
+            if(isSkilledEnough(current))
+                hired = true;
+        }
+        return current;
+    }
+    public boolean isSkilledEnough(Employee emp);
 }
