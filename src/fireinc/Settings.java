42bc0f6e1c46883657fe140fe49f09253ab97900
guus <guus@Guusbook>
2018-06-16 23:09:53 +0200

    added: Settings Made Company initialize all fields Owner now has fields for names Printers now have ID's
    Merge origin/master
    
    Conflicts:
    	src/fireinc/strategies/HRHireStrategy.java
    	src/fireinc/workers/Employee.java

---
+++ src/fireinc/Settings.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fireinc;
+
+/**
+ *
+ * @author guus
+ */
+public class Settings {
+    public static final int NROFPRINTERS =6;
+    
+    public static final int FINMAX = 18;
+    public static final int FINMIN = 10;
+    
+    public static final int PRODMAX = 60;
+    public static final int PRODMIN = 50;
+    
+    public static final int CATMAX = 20;
+    public static final int CATMIN = 25;
+    
+    public static final int HRMAX = 15;
+    public static final int HRMIN = 10;
+    
+    public static final double HR_FEMALE_PREFERENCE = 0.8;
+    
+}
