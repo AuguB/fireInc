18d5d97e8b7aa9b1533e97872274ef9b4f3e20fd
Ron <Ron@10.68.7.254>
2018-06-17 12:09:28 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/fireinc/workers/Intern.java
+++ src/fireinc/workers/Intern.java
@@ -9,6 +9,16 @@ public class Intern<E> extends Employee {
     }
 
     public void run() {
+         while (!fired) {
+            days++;
+            try {
+                work();
+                Thread.sleep(300); //lunchbreak
+                work();
+                Thread.sleep(2000); //sleep
+            } catch (InterruptedException ex) {
+            }
+        }
     }
 
     @Override
@@ -19,6 +29,25 @@ public class Intern<E> extends Employee {
     public void print(int amount) {
     }
 
+    
     public void work() {
+        double result = 0;
+        result += skill 
+               += cleanliness
+               += social
+               += loyalty
+               += experience 
+               += kitchening
+               += 2*looks;
+        
+        if(experience < 1)
+            experience += 0.01;
+        if(skill < 1)
+            skill += 0.01;
+        if(result < 0.6)
+            mistakes++;
+        
+        
+        
     }
 }
